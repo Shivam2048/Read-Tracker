e5c4614499fd15a00ffaece891e5b3ed3e491ff8
Shivam <shivamkr2048@gmail.com>
2025-06-17 18:51:42 +0530

    setup database

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from 'dotenv';
+
+const app= express();
+const port= 2000;
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const db = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: 5432,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+db.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error', err);
+  } else {
+    console.log('Database connected:', res.rows);
+  }
+});
+
