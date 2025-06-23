e1de47667e240ae08b88544b8b15d1aeb9a42bed
Shivam <shivamkr2048@gmail.com>
2025-06-23 08:34:06 +0530

    add API endpoint to retrieve cover_edition_key and connect to PostgreSQL for data fetching

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE books(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     author VARCHAR(50) NOT NULL,
-    cover_url TEXT,
+    cover_edition_key TYPE VARCHAR(11),
     date_read DATE,
     note TEXT,
     rating DECIMAL(2, 1),
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 const app= express();
 const port= 2000;
@@ -21,9 +22,40 @@ const db = new Pool({
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let users = [
-  { id: 1, title: "", author: "", cover_url: "", date_read: "", note: "", rating: "" },
-];
+
+function capitalizeName(str) {
+  return str
+    .trim()
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+async function findOlid(tit, aut){
+  tit = tit.replace(/\s+/g, '+');
+  aut = aut.replace(/\s+/g, '+');
+  try {
+        const response = await axios.get(`https://openlibrary.org/search.json?title=${tit}&author=${aut}`);
+
+        console.log(response.data.docs);
+        if (response.data.docs.length != 0) {
+
+          const result = response.data.docs[0].cover_edition_key;
+          return result;
+
+        } else {
+            console.log("No data found for this book");
+            return null;
+            // res.status(404).send("No data found for this book"); 
+        }
+
+      }catch (error) {
+        console.error("Error in /add route:", error.message);
+        return null;
+        // res.status(500).send("An error occurred while processing your request.");
+    }
+}
 
 
 // db.query('SELECT NOW()', (err, res) => {
@@ -34,12 +66,56 @@ let users = [
 //   }
 // });
 
-app.get('/', (req,res) => {
-  res.render('index.ejs');
+app.get('/', async (req,res) => {
+  let Books = [];
+  let By = "date_read";
+  let Order= "ASC";
+
+  if(req.query.sort){
+    By= req.query.sort;
+    if(By == "date_read" || By == "rating"){
+      Order = "DESC";
+    }
+  }
+
+  try {
+    let query = `SELECT * FROM books ORDER BY ${By} ${Order}`;
+    const result = await db.query(query);
+  } catch (error) {
+    console.log(error);
+  }
+
+  Books = result.rows; 
+
+  res.render('index.ejs', Books);
 });
 
-app.post('/add', (req,res) => {
-  
+app.post('/add', async (req,res) => {
+  let Title= req.body.title;
+  let Author= req.body.author;
+  const dateRead= req.body.DOC;
+  const Note= req.body.summary.trim();
+  const Rating= req.body.rating;
+
+  Title = capitalizeName(Title);
+  Author = capitalizeName(Author);
+
+  const olid= await findOlid(Title,Author);
+  console.log(olid);
+
+  if(!olid){
+    return res.status(404).send("No data found for this book");
+  }
+
+  console.log(Title, Author, olid, dateRead, Note, Rating);
+
+  try {
+    await db.query("INSERT INTO books (title, author, cover_edition_key, date_read, note, rating) VALUES ($1, $2, $3, $4, $5, $6)", [Title, Author, olid, dateRead, Note, Rating]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error inserting data");
+  }
 })
 
 app.listen(port, () => {
